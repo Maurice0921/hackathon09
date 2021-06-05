4ef07cb4e7eef7392a48c64f8df13c43fe254e20
Maurice André Barreto Arbayza <maurice0921@gmail.com>
2021-06-05 13:04:41 -0500

    ejercicios

---
+++ app.js/ejercicios.js
@@ -0,0 +1,90 @@
+/* 01.Implementar un algoritmo que reciba 2 argumentos y los sume, el resultado se deberá imprimir en pantalla.*/
+// const num1 = parseInt(prompt('Ingrese un numero para a:'));
+// const num2 = parseInt(prompt('Ingrese un numero para b:'));
+// const resultado = num1 + num2;
+// console.log(`a+b es igual a:${resultado}`);
+
+// 02.Un estudiante realiza 4 exámenes, calcular el promedio de estos.
+// const nota1 = parseInt(prompt('Ingrese nota del examen 01:'));
+// const nota2 = parseInt(prompt('Ingrese nota del examen 02:'));
+// const nota3 = parseInt(prompt('Ingrese nota del examen 03:'));
+// const nota4 = parseInt(prompt('Ingrese nota del examen 04:'));
+// const resultado = (nota1 + nota2 + nota3 + nota4) / 4;
+// console.log(`el promedio es igual a:${resultado}`);
+
+/*03.Calcular el área de un rectángulo.*/
+// const base = 80;
+// const altura = 50;
+// const area = base * altura;
+// console.log(`El área del rectangulo es:${area}`);
+
+/*04.Calcular el área de un triángulo*/
+// const base = 80;
+// const altura = 50;
+// const area = (base * altura) / 2;
+// console.log(`El área del triangulo es:${area}.`);
+
+/*05.Calcular el área de una circunferencia.*/
+// const radio = 40;
+// const pi = 3.1416;
+// const area = pi * (2 * radio);
+// console.log(`El área de la circunferencia es:${area}.`);
+
+/*06.Determinar el sueldo semanal de un trabajador basándose en sus horas
+trabajadas y su salario de hora hombre.*/
+// const horasTrabajadas = 60;
+// const salarioHoras = 18;
+// const sueldoSemanal = horasTrabajadas * salarioHoras;
+// console.log(`El sueldo semanal del trabajador es de: ${sueldoSemanal} soles.`);
+
+/*Una modista, para realizar sus prendas de vestir, encarga las telas al extranjero. 
+Para cada pedido, tiene que proporcionar las medidas
+de la tela en pulgadas, pero ella generalmente las tiene en metros.
+Realice un algoritmo para ayudar a resolver el problema,
+determinando cuantas pulgadas debe pedir con base en los metros que
+requiere. Represéntelo mediante el diagrama de flujo y el
+pseudocódigo (1 pulgada = 0.0254 m). */
+// const metro = 100;
+// const pulgada = 0.0254;
+// const resultado = metro * pulgada;
+// console.log(`Debe comprar: ${resultado} pulgadas.`);
+
+/*08.Una empresa importadora desea determinar cuántos dólares puede
+adquirir con equis cantidad de dinero peruano.*/
+// const soles = 100;
+// const dolares = 3.87;
+// const conversion = soles * dolares;
+// console.log(`S/.${soles} es equivalente a $${conversion}.`);
+
+/*0.9 Una empresa que contrata personal requiere determinar la edad de las 
+personas que solicitan trabajo, pero cuando se les realiza la entrevista sólo se 
+les pregunta el año en que nacieron*/
+// const personal = {
+// 	nombre: 'Alfonso',
+// 	anioNacimiento: 1985,
+// };
+// const { nombre, anioNacimiento } = personal;
+// const edad = new Date().getFullYear() - anioNacimiento;
+// console.log(`${nombre} tiene ${edad} años`);
+
+/*10.Se tiene el nombre y la edad de tres personas. Se desea saber el nombre y la 
+edad de la persona de menor edad  */
+
+/*11.Se les dará un bono por antigüedad a los empleados de una tienda. Si tienen un 
+año, se les dará $100; si tienen 2 años, $200, y así sucesivamente hasta los 5 
+años. Para los que tengan más de 5, el bono será de $1000. Realice un algoritmo 
+y represéntelo ,que permita determinar el bono que recibirá un trabajador*/
+
+/*14.Una compañía, fabrica focos de colores (verdes, blancos y rojos). Se desea 
+contabilizar, de un lote de N focos, el número de focos de cada color que hay en 
+existencia */
+
+/*15. Realice un algoritmo para determinar si una persona puede votar con base en 
+su edad en las próximas elecciones */
+// const edad = 17;
+// let votante;
+// if (edad >= 18) {
+// 	console.log('Le toca votar el domingo');
+// } else {
+// 	console.log('Aun no tiene edad suficiente para votar');
+// }
